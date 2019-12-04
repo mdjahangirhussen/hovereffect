74b5aa4c675d0a759c88149b9751109c9acb3fa0
mdjahangirhussen <47791938+mdjahangirhussen@users.noreply.github.com>
2019-12-04 11:37:45 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    "use strict";
+
+    
+/*============================================
+	* Author		: # name#
+	* Template Name : name | HTML Template
+	* version		:1.0
+	
+==============================================
+*========== TABLE OF CONTENTS ================
+
+    0.1 WoW js 
+    0.2 Preloader Js
+	0.3 Banner-silder
+	0.4 Testimonial-silder
+	0.5 Brand-silder
+	0.7 Sticky-Header
+    0.8	Use smooth scrolling when clicking on navigation
+	0.9 search-box js
+	10  Dropdown_menu js
+    11 NiceScroll js
+    12 scrolltop js
+    13 Isotope js
+    14 range-slider ui js
+    15 Tooltip js
+    16 Gmaps.js
+
+========================================*/
+//     //-------- 0.  Header bg-color change Js --------
+//    $('.toggle_bar').click( function(){
+//            $('.header-navbar').toggleClass('header-bg');
+//    }); 
+//     //-------- 0.  Navbar-logo change Js --------
+//    $('.toggle_bar').click( function(){
+//        $('.navbar-logo img').toggleClass('logo_black');
+//    }); 
+//    
+//
+//    $('.dropdown .icon').click(function () {
+//    $('.h-ddmenu').slideToggle();
+//});
+//    
+//    $('.icon').click(function () {
+//    $('.icon').toggleClass('active-icon');
+//});
+//     
+//$(".custom_scroll").niceScroll({
+//    autohidemode: false,
+//});
+//});
+//
+//
+//
+// $(function () {
+//     // isotope js 
+//     var $grid = $('.choose_demo').isotope({
+//         itemSelector: '.grid-item',
+//         percentPosition: true,
+//         layoutMode: 'masonry'
+//     });
+// });
+//
+////$('.choose_demo').isotope({
+////  itemSelector: '.grid-item',
+////  percentPosition: true,
+////  layoutMode: 'masonry'
+////});
+//
+// $(function () {
+//     // filter-button-group
+//
+//     $('.filter-button-group').on('click', 'button', function () {
+//         var filterValue = $(this).attr('data-filter');
+//         $('.grid').isotope({
+//             filter: filterValue
+//         });
+//     });
+//
+// });
+//
+//
+//
+//    //-------- 0.2 Fixed Top Navbar Js --------
+//    $(window).scroll(function () {
+//        var sticky = $('.sticky'),
+//            scroll = $(window).scrollTop();
+//
+//        if (scroll >= 180) sticky.addClass('fixed')
+//        else sticky.removeClass('fixed');
+//    });
+//
+//
+// $(".dropdown").hover(
+//        function () {
+//            $(this).find('.d-down-menu').stop(true, false, true).slideDown(500);
+//        },
+//        function () {
+//            $(this).find('.d-down-menu').stop(true, false, true).slideUp(100);
+//});
+
+
+
+
